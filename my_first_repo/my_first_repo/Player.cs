a8d5c7806418c97372c21ed2ebc47bcf53a93875
unknown <o018313c@k106-14.student.staffs.ac.uk>
2014-10-01 11:42:36 +0100

    Revert "Revert "Why Not""
    This reverts commit 99943658d23b52ab5fc96aaebc2857fd4a4642da.

---
+++ my_first_repo/my_first_repo/Player.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace my_first_repo
+{
+	public class Player
+	{
+		string name;
+		public int score;
+		public string xPos; 
+		public string yPos;
+		
+		public Player ()
+		{
+			
+		
+		}
+		
+		public void GetName()
+		{
+		}
+		
+		public void GetScore()
+		{
+		}
+		
+		public int GetXPos()
+		{
+			return xPos;
+		}
+		
+		public int GetYPos()
+		{
+			return yPos;
+		}
+	}
+}
+
